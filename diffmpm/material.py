fe77bcf4d954ffca438b1edabc6e59405e7552ec
Sachin Jalan <sachinjalan03@gmail.com>
2023-07-02 18:55:41 +0530

    Bingham Material

--- diffmpm/material.py
+++ diffmpm/material.py
@@ -132,7 +132,76 @@ class SimpleMaterial(Material):
     def compute_stress(self, dstrain):
         return dstrain * self.properties["E"]
 
-#commenting to create bingham model
+
+# commenting to create bingham model
+@register_pytree_node_class
+class Bingham(Material):
+    _props = (
+        "density",
+        "youngs_modulus",
+        "poisson_ratio",
+        "tau0",
+        "mu_",
+        "critical_shear_rate",
+    )
+
+    def __init__(self, material_properties):
+        self.validate_props(material_properties)
+        density = material_properties["density"]
+        youngs_modulus = material_properties["youngs_modulus"]
+        poisson_ratio = material_properties["poisson_ratio"]
+        tau0 = material_properties["tau0"]
+        mu_ = material_properties["mu_"]
+        critical_shear_rate = material_properties["critical_shear_rate"]
+        bulk_modulus = youngs_modulus / (3.0 * (1.0 - 2.0 * poisson_ratio))
+        compressibility_multiplier_=1.0
+        if(material_properties.has_key("incompressible")):
+            incompressible = material_properties["incompressible"]
+            if(incompressible):
+                compressibility_multiplier_=0.0
+        self.properties = {
+            **material_properties,
+            "bulk_modulus": bulk_modulus,
+            "compressibility_multiplier": compressibility_multiplier_,
+        }
+    
+    def __repr__(self):
+        return f"Bingham(props={self.properties})"
+    
+    def thermodynamic_pressure(self,volumetric_strain):
+        return -self.properties["bulk_modulus"]*volumetric_strain
+    
+    def compute_stress(self, dstrain, particle):
+
+        def compute_stress_per_particle(i):
+            strain_rate=particle.strain_rate[i]
+            strain_rate=strain_rate.at[-3:].set(strain_rate[-3:]*0.5)
+            strain_rate_threshold=1e-15
+            if(self.critical_shear_rate<shear_rate_threshold):
+                self.critical_shear_rate=shear_rate_threshold
+            shear_rate=jnp.sqrt(2.0*(strain_rate.T@(strain_rate) + strain_rate[-3].T@strain_rate[-3,jnp.newaxis]))
+            apparent_viscosity=0.0
+            if(shear_rate[0,0]*shear_rate[0,0] > self.critical_shear_rate):
+                apparent_viscosity=2.0*((self.tau0/shear_rate[0,0]))
+            tau=apparent_viscosity*strain_rate
+            trace_invariant2=0.5*jnp.dot(tau[:,0],tau[:,0])
+            if(trace_invariant2<(self.tau0*self.tau0)):
+                tau=tau.at[:].set(0)
+            
+            
+
+        strain_rate=particle.strain_rate
+        #Convert strain rate to rate of deformation tensor
+        strain_rate[:,-3:,:]*=0.5
+        shear_rate_threshold=1e-15
+        if(self.critical_shear_rate<shear_rate_threshold):
+            self.critical_shear_rate=shear_rate_threshold
+        shear_rate=jnp.sqrt(strain_rate@(strain_rate.transpose(0,2,1))+ (strain_rate[:,-3,:]@(strain_rate[:,-3,:].transpose(0,2,1))))
+
+
+
+        
+
 if __name__ == "__main__":
     from diffmpm.utils import _show_example
 
