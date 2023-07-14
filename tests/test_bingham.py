4524aa187301f2fa3b6459517017e3c95644e20a
Sachin Jalan <sachinjalan03@gmail.com>
2023-07-14 20:00:41 +0530

    Necessary changes done

--- tests/test_bingham.py
+++ tests/test_bingham.py
@@ -6,20 +6,8 @@ from diffmpm.element import Quadrilateral4Node
 from diffmpm.constraint import Constraint
 from diffmpm.node import Nodes
 
-material_dstrain_particles_state_vars_element_targets_dt = [
+particles_element_targets = [
     (
-        Bingham(
-            {
-                "density": 1000,
-                "youngs_modulus": 1.0e7,
-                "poisson_ratio": 0.3,
-                "tau0": 771.8,
-                "mu": 0.0451,
-                "critical_shear_rate": 0.2,
-                "ndim": 2,
-            }
-        ),
-        None,
         Particles(
             jnp.array([[0.5, 0.5]]).reshape(1, 1, 2),
             (
@@ -37,7 +25,6 @@ material_dstrain_particles_state_vars_element_targets_dt = [
             ),
             jnp.array([0]),
         ),
-        {"pressure": jnp.zeros(1)},
         Quadrilateral4Node(
             (1, 1),
             1,
@@ -46,21 +33,8 @@ material_dstrain_particles_state_vars_element_targets_dt = [
             Nodes(4, jnp.array([-2, -2, 2, -2, -2, 2, 2, 2]).reshape((4, 1, 2))),
         ),
         jnp.array([0.0, 0.0, 0.0, 0.0, 0.0, 0.0]).reshape((6, 1)),
-        1.0,
     ),
     (
-        Bingham(
-            {
-                "density": 1000,
-                "youngs_modulus": 1.0e7,
-                "poisson_ratio": 0.3,
-                "tau0": 771.8,
-                "mu": 0.0451,
-                "critical_shear_rate": 0.2,
-                "ndim": 2,
-            }
-        ),
-        None,
         Particles(
             jnp.array([[0.5, 0.5]]).reshape(1, 1, 2),
             (
@@ -78,7 +52,6 @@ material_dstrain_particles_state_vars_element_targets_dt = [
             ),
             jnp.array([0]),
         ),
-        {"pressure": jnp.zeros(1)},
         Quadrilateral4Node(
             (1, 1),
             1,
@@ -89,21 +62,8 @@ material_dstrain_particles_state_vars_element_targets_dt = [
         jnp.array([-52083.3333333333, -52083.3333333333, 0.0, 0.0, 0.0, 0.0]).reshape(
             (6, 1)
         ),
-        1.0,
     ),
     (
-        Bingham(
-            {
-                "density": 1000,
-                "youngs_modulus": 1.0e7,
-                "poisson_ratio": 0.3,
-                "tau0": 200.0,
-                "mu": 200.0,
-                "critical_shear_rate": 0.2,
-                "ndim": 2,
-            }
-        ),
-        None,
         Particles(
             jnp.array([[0.5, 0.5]]).reshape(1, 1, 2),
             (
@@ -121,7 +81,6 @@ material_dstrain_particles_state_vars_element_targets_dt = [
             ),
             jnp.array([0]),
         ),
-        {"pressure": jnp.zeros(1)},
         Quadrilateral4Node(
             (1, 1),
             1,
@@ -132,22 +91,8 @@ material_dstrain_particles_state_vars_element_targets_dt = [
         jnp.array(
             [-5208520.35574006, -5208613.86694342, 0.0, -233.778008402801, 0.0, 0.0]
         ).reshape((6, 1)),
-        1.0,
     ),
     (
-        Bingham(
-            {
-                "density": 1000,
-                "youngs_modulus": 1.0e7,
-                "poisson_ratio": 0.3,
-                "tau0": 200.0,
-                "mu": 200.0,
-                "critical_shear_rate": 0.2,
-                "ndim": 2,
-                "incompressible": True,
-            }
-        ),
-        None,
         Particles(
             jnp.array([[0.5, 0.5]]).reshape(1, 1, 2),
             (
@@ -166,7 +111,6 @@ material_dstrain_particles_state_vars_element_targets_dt = [
             ),
             jnp.array([0]),
         ),
-        {"pressure": jnp.zeros(1)},
         Quadrilateral4Node(
             (1, 1),
             1,
@@ -177,18 +121,28 @@ material_dstrain_particles_state_vars_element_targets_dt = [
         jnp.array(
             [-187.0224067222, -280.5336100834, 0.0, -233.778008402801, 0.0, 0.0]
         ).reshape((6, 1)),
-        1.0,
     ),
 ]
 
 
+@pytest.fixture
+def state_vars():
+    return {"pressure": jnp.zeros(1)}
+
+
+@pytest.fixture
+def dt():
+    return 1.0
+
+
 @pytest.mark.parametrize(
-    "material, dstrain, particles, state_vars, element, target, dt",
-    material_dstrain_particles_state_vars_element_targets_dt,
+    "particles, element, target",
+    particles_element_targets,
 )
-def test_compute_stress(material, dstrain, particles, state_vars, element, target, dt):
-    if element.constraints != []:
+def test_compute_stress(particles, state_vars, element, target, dt):
+    particles.update_natural_coords(element)
+    if element.constraints:
         element.apply_boundary_constraints()
     particles.compute_strain(element, dt)
-    stress = material.compute_stress(dstrain, particles, state_vars)
+    stress = particles.material.compute_stress(None, particles, state_vars)
     assert jnp.allclose(stress, target)
